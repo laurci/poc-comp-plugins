0acfaabb60698e2985af72d6c2c35541b0578e31
Ciobanu Laurentiu <laurentiu.ciobanu@flowos.com>
2022-07-03 14:54:13 +0300

    fix: line plugin wrong line number (0 based)

--- src/plugins/line-plugin.ts
+++ src/plugins/line-plugin.ts
@@ -4,7 +4,7 @@ export default createPlugin((plugin) => {
     plugin.match(ts.isIdentifier, (it) => {
         if (it.node.text == "__line") {
             const {line} = it.node.getSourceFile().getLineAndCharacterOfPosition(it.node.getStart());
-            return it.replace(it.ctx.factory.createNumericLiteral(line));
+            return it.replace(it.ctx.factory.createNumericLiteral(line + 1));
         }
     });
 });
